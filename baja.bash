391974d6cfe127496918d127667ad10960c89553
IILHPII <l.hernandez071999@gmail.com>
2022-11-20 04:01:25 -0300

    Mas arreglos

--- baja.bash
+++ baja.bash
@@ -7,13 +7,19 @@ clear
  echo
 
  read -p "Ingrese numero de ticket a buscar: " ticket
+ cedula=$(grep "^$ticket:" faltas.txt | cut -d ":" -f2)
+ usuario=$USER
+ lineaTicket=$(grep -n "^$ticket:" faltas.txt | cut -d: -f1)
+ fecha=$(date +%Y/%m/%d)
+ 
     if grep -q "^$ticket:" faltas.txt
     then
         grep "^$ticket:" faltas.txt>>bajas.txt
-        echo "La falta que ha sido borrada es: " $( grep "^$ticket:" lista.txt )
-      
-      grep -v "^$ticket:" faltas.txt > temp
-      mv temp lista.txt
+        echo $fecha":""El usuario" $usuario "dio de baja al registro de falta con numero de ticket " $ticket "de la cedula "$cedula >>registros.log
+        grep -v "^$ticket:" faltas.txt > temp
+        mv temp faltas.txt
+        echo "La falta que ha sido borrada es: " $( grep "^$ticket:" bajas.txt )
+        sleep 5
     else
     echo No existe el registro.
     fi
